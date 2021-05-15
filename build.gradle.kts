ef96baea072db12c2153a190ad73966bbb88a40c
Lorenzo Guimarães <lorenzooguimaraes@gmail.com>
2021-05-15 17:57:52 -0300

    build: add detekt and update ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import com.lorenzoog.plank.build.Dependencies
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 
 buildscript {
   repositories {
     maven("https://jitpack.io")
+    mavenCentral()
   }
 
   dependencies {
@@ -11,17 +13,27 @@ buildscript {
 }
 
 plugins {
-  kotlin("multiplatform") version "1.4.30-M1" apply false
-  id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
-
+  kotlin("multiplatform") version "1.4.32" apply false
+  id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+  id("io.gitlab.arturbosch.detekt") version "1.16.0"
   id("composite-build")
 }
 
 group = "com.lorenzoog"
 version = "1.0-SNAPSHOT"
 
+repositories {
+  mavenCentral()
+  jcenter()
+}
+
+dependencies {
+  ktlintRuleset(Dependencies.Pinterest.Ktlint)
+}
+
 subprojects {
   apply(plugin = "org.jlleitschuh.gradle.ktlint")
+  apply(plugin = "io.gitlab.arturbosch.detekt")
   apply(plugin = "composite-build")
 
   repositories {
@@ -36,7 +48,22 @@ subprojects {
   }
 
   ktlint {
-    android.set(true)
+    android.set(false)
+  }
+
+  detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+
+    config = files("${rootProject.projectDir}/config/detekt.yml")
+    baseline = file("${rootProject.projectDir}/config/baseline.xml")
+
+    reports {
+      html.enabled = true
+      xml.enabled = true
+      txt.enabled = true
+      sarif.enabled = true
+    }
   }
 
   tasks.withType<KotlinJvmCompile> {
