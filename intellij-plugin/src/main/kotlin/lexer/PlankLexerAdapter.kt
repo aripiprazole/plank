1250e8a2854d6e03244fce4dacbb9abe08f13956
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-05-16 20:40:46 -0300

    refactor: remove old intellij plugin

--- intellij-plugin/src/main/kotlin/lexer/PlankLexerAdapter.kt
+++
@@ -1,5 +0,0 @@
-package com.lorenzoog.jplank.intellijplugin.lexer
-
-import com.intellij.lexer.FlexAdapter
-
-class PlankLexerAdapter : FlexAdapter(IdeaPlankLexer(null))
--- intellij-plugin/src/main/kotlin/psi/PsiScope.kt
+++
@@ -1,6 +0,0 @@
-package com.lorenzoog.jplank.intellijplugin.psi
-
-import com.intellij.psi.PsiElement
-import com.lorenzoog.jplank.intellijplugin.analyzer.Scope
-
-interface PsiScope : PsiElement, Scope
