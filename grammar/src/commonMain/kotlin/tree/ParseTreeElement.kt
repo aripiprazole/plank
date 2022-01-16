9f18f74036d09ccc00db28417d88f4c6186c4a30
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-01-16 08:52:26 -0300

    feat: add parse tree for debugging

---
+++ grammar/src/commonMain/kotlin/tree/ParseTreeElement.kt
@@ -0,0 +1,48 @@
+package com.gabrielleeg1.plank.grammar.tree
+
+import org.antlr.v4.kotlinruntime.ParserRuleContext
+import org.antlr.v4.kotlinruntime.tree.TerminalNode
+import pw.binom.collection.LinkedList
+
+sealed interface ParseTreeElement {
+  override fun toString(): String
+
+  fun multilineString(ident: String = ""): String
+}
+
+class ParseTreeLeaf(val text: String) : ParseTreeElement {
+  override fun toString(): String = "T[$text]"
+
+  override fun multilineString(ident: String): String = ident + "T[$text]\n"
+}
+
+class ParseTreeNode(val name: String): ParseTreeElement {
+  val children = LinkedList<ParseTreeElement>()
+
+  fun child(element: ParseTreeElement): ParseTreeNode {
+    children.add(element)
+    return this
+  }
+
+  override fun toString(): String = "Node($name) $children"
+
+  override fun multilineString(ident: String): String = buildString {
+    append(ident)
+    append(name)
+    append("\n")
+    children.forEach { element ->
+      append(element.multilineString("$ident  "))
+    }
+  }
+}
+
+fun ParserRuleContext.toParseTree(): ParseTreeNode {
+  val tree = ParseTreeNode(this::class.simpleName ?: error("Unknown name for parse context: $this"))
+  children.orEmpty().forEach { element ->
+    when (element) {
+      is ParserRuleContext -> tree.child(element.toParseTree())
+      is TerminalNode -> tree.child(ParseTreeLeaf(element.text))
+    }
+  }
+  return tree
+}
