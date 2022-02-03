0b35bbead38b899c19f045d7559a52f8e9a67082
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-02-02 22:35:23 -0300

    refactor: remove old jvm compiler

--- compiler/src/jvmMain/kotlin/instructions/CompilerInstruction.kt
+++
@@ -1,8 +0,0 @@
-package com.gabrielleeg1.plank.compiler.instructions
-
-import com.gabrielleeg1.plank.compiler.CompilerContext
-import org.llvm4j.llvm4j.Value
-
-interface CompilerInstruction {
-  fun CompilerContext.codegen(): Value
-}
--- compiler/src/jvmMain/kotlin/instructions/element/IRElement.kt
+++
@@ -1,5 +0,0 @@
-package com.gabrielleeg1.plank.compiler.instructions.element
-
-import com.gabrielleeg1.plank.compiler.instructions.CompilerInstruction
-
-sealed interface IRElement : CompilerInstruction
