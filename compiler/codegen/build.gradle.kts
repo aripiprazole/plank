6aac9f0015932e877d0d6b2d9a2be18c1acd9efa
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-07-23 23:20:34 -0300

    refactor(codegen): remove native modules

--- compiler/codegen/build.gradle.kts
+++ compiler/codegen/build.gradle.kts
@@ -1,28 +1,4 @@
-import org.jetbrains.kotlin.gradle.testing.KotlinTaskTestRun
-import org.plank.build.LlvmConfig
-
 kotlin {
-  val linuxX64 = linuxX64("linuxX64")
-  val mingwX64 = mingwX64("mingwX64")
-
-  configure(listOf(linuxX64, mingwX64)) {
-    val test: KotlinTaskTestRun<*, *> by testRuns
-
-    test.executionTask.configure {
-      testLogging.showStandardStreams = true
-    }
-
-    binaries {
-      getTest("debug").apply {
-        if (LlvmConfig.hasLlvm()) {
-          linkerOpts.addAll(LlvmConfig.cmd("--ldflags").split(" ").filter { it.isNotBlank() })
-          linkerOpts.addAll(LlvmConfig.cmd("--system-libs").split(" ").filter { it.isNotBlank() })
-          linkerOpts.addAll(LlvmConfig.cmd("--libs").split(" ").filter { it.isNotBlank() })
-        }
-      }
-    }
-  }
-
   sourceSets {
     all {
       languageSettings.optIn("kotlin.RequiresOptIn")
@@ -58,18 +34,6 @@ kotlin {
         implementation(libs.kt.test.junit)
         implementation(libs.jupiter.api)
         implementation(libs.jupiter.engine)
-      }
-    }
-
-    val linuxX64Main by getting {
-      dependencies {
-        implementation(projects.compiler.llvm4k)
-      }
-    }
-
-    val mingwX64Main by getting {
-      dependencies {
-        implementation(projects.compiler.llvm4k)
       }
     }
   }
