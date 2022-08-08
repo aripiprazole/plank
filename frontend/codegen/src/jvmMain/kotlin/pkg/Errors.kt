e5cb7d814c8e6e3cf0b8ff918051c556b1d9cbe5
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-08-08 06:27:59 -0300

    refactor(codegen): remove  module

--- frontend/codegen/src/jvmMain/kotlin/pkg/Errors.kt
+++
@@ -1,8 +0,0 @@
-package org.plank.codegen.pkg
-
-import org.plank.analyzer.checker.CheckViolation
-import org.plank.syntax.SyntaxViolation
-
-class SyntaxError(val violations: Set<SyntaxViolation>) : RuntimeException()
-
-class AnalyzerError(val violations: Set<CheckViolation>) : RuntimeException()
