292d622319c1acff396a319cfe19dd1ad5fe3fea
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-08-22 11:11:43 -0300

    build: use configuration cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "plank"
 
 enableFeaturePreview("VERSION_CATALOGS")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 
 pluginManagement {
   repositories {
